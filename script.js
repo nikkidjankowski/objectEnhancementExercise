f9a04e10839bb332a3d9474c5762de4ce6aaa235
nikkidjankowski <79601782+nikkidjankowski@users.noreply.github.com>
2022-02-17 15:52:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+function createInstructor(firstName, lastName){
+    return {
+      firstName,
+      lastName
+    }
+  }
+
+let favoriteNumber = 42;
+
+const instructor = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!"
+}
+
+const instructor = {
+    firstName: "Colt",
+    sayHi(){
+      return "Hi!";
+    },
+    sayBye(){
+      return this.firstName + " says bye!";
+    }
+  }
+*/
+
+
+const d = createAnimal("dog", "bark", "Woooof!");
+const h = createAnimal("horse", "neigh", "yeeehaww!");
+
+function createAnimal(species, verb, noise){
+    return{
+        species,
+        [verb](){
+            return noise;
+        }
+    }
+}
+
